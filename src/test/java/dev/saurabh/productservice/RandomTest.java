541de35672d117428944a7d28c89c3caec16c952
saurabhkaushik <saurabh.kaushiksde@gmail.com>
2023-12-19 12:01:52 +0530

    Random test case

---
+++ src/test/java/dev/saurabh/productservice/RandomTest.java
@@ -0,0 +1,19 @@
+package dev.saurabh.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Random;
+
+@SpringBootTest
+public class RandomTest {
+
+    @Test
+    void testLessThan3(){
+        Random random = new Random();
+        int number = random.nextInt();
+
+        assert(number < 100000);
+    }
+
+}
